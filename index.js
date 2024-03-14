295503b2c06c7c7deff9e52675c972371df725bb
mouni2619 <mounikaadada26@gmail.com>
2024-03-14 17:00:34 +0530

    updated

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ import dotenv from 'dotenv';
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
-const uri = process.env.MONGODB_URI || 'mongodb://localhost:27017';
+const uri = process.env.MONGODB_URI
+
 const client = new MongoClient(uri);
 
 let db;
